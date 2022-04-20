95f2f54191ed9dd7c2176ebb9ca5fb24b2812008
root <root@h3.robocup.org>
2022-04-20 11:13:59 +0200

    fix issues

---
+++ web/replace_icon.sh
@@ -0,0 +1,6 @@
+cp /opt/mailman-web-data/logo/favicon.ico /opt/mailman-web-data/static/postorius/img/favicon.ico |true
+cp /opt/mailman-web-data/logo/logo.png /opt/mailman-web-data/static/postorius/img/mailman_logo.png |true
+cp /opt/mailman-web-data/logo/logo.png /opt/mailman-web-data/static/postorius/img/mailman_logo_trans.png |true
+cp /opt/mailman-web-data/logo/logo_small.png /opt/mailman-web-data/static/postorius/img/mailman_logo_small_trans.png |true
+cp /opt/mailman-web-data/logo/logo.png /opt/mailman-web-data/static/hyperkitty/img/logo.png |true
+cp /opt/mailman-web-data/logo/favicon.ico /opt/mailman-web-data/static/hyperkitty/img/favicon.ico |true
